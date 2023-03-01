a06366cdba968a21a39e2d10c3372834d417063c
mocchio <skmt.rome@gmail.com>
2023-03-01 16:53:04 +0900

    アクションとルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [:index, :create, :new, :show]
+  resources :prototypes, only: [:index, :create, :new, :show] do
+    resources :comments, only: :create
+  end
   root to: 'prototypes#index'
   resources :users, only: :show
 end
