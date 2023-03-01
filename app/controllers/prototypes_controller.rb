f85927907e7aaf85140811bcc573c5c8dcaf0e50
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-01 10:39:55 +0900

    修正しました

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,6 @@ class PrototypesController < ApplicationController
   
   private 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image, ).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
 end
