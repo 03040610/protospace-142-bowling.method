127cf580455555cfc0023b0cec8efec12eceeb15
03040610 <yukikobayashi0304@gmail.com>
2023-03-01 17:06:32 +0900

    削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [:index, :create, :new, :show]
+  resources :prototypes, only: [:index, :create, :new, :show, :destroy]
   root to: 'prototypes#index'
   resources :users, only: :show
 end
